09f9d02dfdcec1bab678159554b02ba74d2c8404
Dong Liu <edongliu@gmail.com>
2012-12-20 21:14:44 -0500

    add average

--- notification/sim_orch.js
+++ notification/sim_orch.js
@@ -1,10 +1,11 @@
 var fs = require('fs'), 
   sim = require('./sim'),
-  sim_event,
-  // Numbers = require('numbers'),
-	config,
-	job_size,
-  // current, 
+  network_event,
+  simulator,
+  // config,
+	config = JSON.parse(fs.readFileSync(__dirname + '/network_3_1.json')),
+	job_size = 1000,
+  current, 
   network_state, 
   event_source = [],
   event_name,
@@ -12,13 +13,13 @@ var fs = require('fs'),
   fail,
   stats = []; // all stats here
 
-if (process.argv.length === 4) {
-  config = JSON.parse(fs.readFileSync(__dirname + '/' + process.argv[2]));
-  job_size = parseInt(process.argv[3], 10);
-} else {
-  console.warn('wrong number of arguments.');
-  return;
-}
+// if (process.argv.length === 4) {
+//   config = JSON.parse(fs.readFileSync(__dirname + '/' + process.argv[2]));
+//   job_size = parseInt(process.argv[3], 10);
+// } else {
+//   console.warn('wrong number of arguments.');
+//   return;
+// }
 
 console.log(config);
 console.log(job_size);
@@ -39,22 +40,25 @@ for (i = 0; i < config.partition; i += 1) {
   waiting[i] = [];
 }
 
-console.log(event_source);
+// console.log(event_source);
 
-sim_event = new sim.Sim;
+simulator = new sim.Sim;
+network_event = new sim.Sim;
+// arrival_event = new sim.Sim;
 
-sim_event.on('arrival', function(current){
+// sim_event.on('arrival', function(current){
+network_event.on('arrival', function(){
   // console.log('arrival at ' + current);
   job_size = job_size - 1;
   // current = time;
   for (var j = 0; j < config.service; j += 1) {
-    stats.push(nextPareto(config.min/10, config.shape));
+    // stats.push(nextBoundedPareto(config.min/10, config.max/10, config.shape));
     success += 1;
   }
   for(var i = 0; i < config.partition; i += 1) {
     if (network_state[i] === 1) {
       for (j = 0; j < config.service; j += 1) {
-        stats.push(nextPareto(config.min, config.shape));
+        // stats.push(nextBoundedPareto(config.min, config.max, config.shape));
         success += 1;
       }
     } else {
@@ -62,71 +66,71 @@ sim_event.on('arrival', function(current){
     }
   }
   event_source[0] = current + nextExp(config.rate);
-  // process.nextTick(sim_event.emit('next'));
-  sim_event.emit('next');
+  process.nextTick(simulator.emit('next'));
+  // simulator.emit('next', 'arrival');
 });
 
-sim_event.on('1-2', function(current){
-  console.log(network_state);
-  console.log('connection 1-2 changes at ' + current);
-  // current = time;
+// sim_event.on('1-2', function(current){
+network_event.on('1-2', function(){
+  // console.log(network_state);
+  // console.log('connection 1-2 changes at ' + current);
   if (network_state[0] === 1) {
     network_state[0] = 0;
     event_source[1] = current + nextGaussian(config.mttr_mu, config.sigma);
   } else {
-    console.log(waiting);
+    // console.log(waiting);
     network_state[0] = 1;
     for (var i = 0; i < waiting[0].length; i += 1) {
       if (current < waiting[0][i]) {
         break;
       }
     }
-    console.log('failed ' + i);
+    // console.log('failed ' + i);
     fail += i * config.service;
     for (var j = 0; j < waiting[0].length - i; j += 1) {
       for (var k = 0; k < config.service; k += 1) {
-        stats.push(current - waiting[0][j] + config.timeout + nextPareto(config.min, config.shape));
+        // stats.push(current - waiting[0][j] + config.timeout + nextPareto(config.min, config.shape));
         success += 1;
       } 
     }
     waiting[0] = []; // clear waiting list
     event_source[1] = current + nextGaussian(config.mttf_mu, config.sigma);
   }
-  // process.nextTick(sim_event.emit('next'));
-  sim_event.emit('next');
+  process.nextTick(simulator.emit('next'));
+  // simulator.emit('next', '1-2');
 });
 
-sim_event.on('1-3', function(current){
-  console.log(network_state);
-  console.log('connection 1-3 changes at ' + current);
-  // current = time;
+// sim_event.on('1-3', function(current){
+network_event.on('1-3', function(){
+  // console.log(network_state);
+  // console.log('connection 1-3 changes at ' + current);
   if (network_state[1] === 1) {
     network_state[1] = 0;
     event_source[2] = current + nextGaussian(config.mttr_mu, config.sigma);
   } else {
-    console.log(waiting);
+    // console.log(waiting);
     network_state[1] = 1;
     for (var i = 0; i < waiting[1].length; i += 1) {
       if (current < waiting[1][i]) {
         break;
       }
     }
-    console.log('failed ' + i);
+    // console.log('failed ' + i);
     fail += i * config.service;
     for (var j = 0; j < waiting[1].length - i; j += 1) {
       for (var k = 0; k < config.service; k += 1) {
-        stats.push(current - waiting[1][j] + config.timeout + nextPareto(config.min, config.shape));
+        // stats.push(current - waiting[1][j] + config.timeout + nextPareto(config.min, config.shape));
         success += 1;
       } 
     }
     waiting[1] = []; // clear waiting list
     event_source[2] = current + nextGaussian(config.mttf_mu, config.sigma);
   }
-  // process.nextTick(sim_event.emit('next'));
-  sim_event.emit('next')
+  process.nextTick(simulator.emit('next'));
+  // simulator.emit('next', '1-3')
 });
 
-sim_event.on('next', function(){
+simulator.on('next', function(){
   // console.log('next');
   var min = minimum(event_source);
   // console.log(event_source);
@@ -138,17 +142,36 @@ sim_event.on('next', function(){
         console.log('done');
         console.log('fail: ' + fail);
         console.log('success: ' + success);
-        console.log(event_source);
-        console.log(waiting);
+        // console.log(event_source);
+        // console.log(waiting);
         console.log('stats: ' + stats.length);
         // console.log('stats: ' + stats);
-        sim_event.removeAllListeners();
+        simulator.removeAllListeners();
+        network_event.removeAllListeners();
 
         process.exit(0);
       } else {
         // process.nextTick(sim_event.emit(event_name[i], min));
-        console.log('current is ' + min);
-        sim_event.emit(event_name[i], min)
+        // console.log('current is ' + min);
+        // sim_event.emit(event_name[i], min);
+        current = min;
+        // switch (i) {
+        //   case 0: {
+        //     sim_event.emit('arrival');
+        //     break;
+        //   }
+        //   case 1: {
+        //     sim_event.emit('1-2');
+        //     break;
+        //   }
+        //   case 2: {
+        //     sim_event.emit('1-3');
+        //     break;
+        //   }
+
+        // }
+        // network_event.emit(event_name[i], 'next');
+        process.nextTick(network_event.emit(event_name[i]));
       }
         
     }
@@ -156,7 +179,7 @@ sim_event.on('next', function(){
 });
 
 
-sim_event.emit('next');
+simulator.emit('next');
 
 
 function minimum(array) { 
---
+++ notification/sim_orch_cluster.js
@@ -0,0 +1,197 @@
+var fs = require('fs'), 
+  cluster = require('cluster'),
+  worker,
+  config,
+	// config = JSON.parse(fs.readFileSync(__dirname + '/network_3_1.json')),
+  // job_size = 10000,
+	job_size,
+  network_state, 
+  event_source = [],
+  event_name,
+  waiting,
+  fail,
+  stats = []; // all stats here
+
+if (process.argv.length === 4) {
+  config = JSON.parse(fs.readFileSync(__dirname + '/' + process.argv[2]));
+  job_size = parseInt(process.argv[3], 10);
+} else {
+  console.warn('wrong number of arguments.');
+  return;
+}
+
+
+if (cluster.isMaster){
+  // init cluster
+  event_source.push(nextExp(config.rate));
+  event_source.push(Math.random()*nextGaussian(config.mttf_mu, config.sigma));
+  event_source.push(Math.random()*nextGaussian(config.mttf_mu, config.sigma));
+  event_name = ['arrival', '1-2', '1-3'];
+  console.log(config);
+  console.log(job_size);
+  worker = cluster.fork();
+  cluster.on('exit', function(worker, code, signal) {
+    console.log('worker ' + worker.process.pid + ' died');
+  });
+  worker.on('message', function(msg) {
+    // console.log(msg);
+    event_source[msg.type] = msg.update;
+    var index = minIndex(event_source);
+    if (job_size <= 0 && index === 0) { // jobs finished
+      console.log('done');
+      worker.send({'type': 'done'});
+    } else {
+      if (index === 0) {
+        job_size -= 1;
+      }
+      worker.send({'type': event_name[index], 'current': event_source[index]});
+    }
+
+  });
+  // start the simulation 
+  var start = minIndex(event_source);
+  job_size -= 1;
+  worker.send({'type': event_name[start], 'current': event_source[start]});
+} else if (cluster.isWorker) {
+  // init worker
+  fail = 0;
+  success = 0;
+  network_state = [1, 1];
+  waiting = new Array(config.partition);
+  for (i = 0; i < config.partition; i += 1) {
+    waiting[i] = [];
+  }
+  process.on('message', function(msg){
+    // console.log(msg);
+    var update;
+    switch (msg.type) {
+      case 'arrival': {
+        update = updateArrival(msg.current);
+        process.send({'type': 0, 'update': update});
+        break;
+      }
+      case '1-2': {
+        update = updateNetwork(msg.current, 0);
+        process.send({'type': 1, 'update': update});
+        break;
+      }
+      case '1-3': {
+        update = updateNetwork(msg.current, 1);
+        process.send({'type': 2, 'update': update});
+        break;
+      }
+      case 'done': {
+        console.log('fail: ' + fail);
+        console.log('success: ' + success);
+        console.log('average: ' + average(stats));
+        process.exit(0);
+        break;
+      }
+    }
+  });
+}
+
+function updateArrival(current) {
+  for (var j = 0; j < config.service; j += 1) {
+    stats.push(nextBoundedPareto(config.min/10, config.max/10, config.shape));
+    success += 1;
+  }
+  for(var i = 0; i < config.partition; i += 1) {
+    if (network_state[i] === 1) {
+      for (j = 0; j < config.service; j += 1) {
+        stats.push(nextBoundedPareto(config.min, config.max, config.shape));
+        success += 1;
+      }
+    } else {
+      waiting[i].push(current+config.timeout);
+    }
+  }
+  return current + nextExp(config.rate);
+}
+
+function updateNetwork(current, interface) {
+  console.log('network: ' + network_state + ' current: ' + current + ' interface: ' + interface);
+  if (network_state[interface] === 1) {
+    network_state[interface] = 0;
+    return current + nextGaussian(config.mttr_mu, config.sigma);
+  } else {
+    console.log(waiting);
+    network_state[interface] = 1;
+    for (var i = 0; i < waiting[interface].length; i += 1) {
+      if (current < waiting[interface][i]) {
+        break;
+      }
+    }
+    console.log('failed ' + i);
+    fail += i * config.service;
+    for (var j = 0; j < waiting[interface].length - i; j += 1) {
+      for (var k = 0; k < config.service; k += 1) {
+        stats.push(current - waiting[0][j] + config.timeout + nextBoundedPareto(config.min, config.max, config.shape));
+        success += 1;
+      } 
+    }
+    waiting[interface] = []; // clear waiting list
+    return current + nextGaussian(config.mttf_mu, config.sigma);
+  }
+}
+
+function minIndex(array) { 
+  if (array.length === 0) {
+    return null;
+  }
+  if (array.length === 1) {
+    return 0;
+  }
+  var i;
+  var min = array[0], position = 0;
+  for (i = 1; i < array.length; i += 1) {
+    if (min > array[i]) {
+      min = array[i];
+      position = i;
+    }
+  }
+  return position;
+}
+
+function average(array) {
+  return array.reduce(function(p, c, i, a) {
+    return (p * i + c) / (i + 1); 
+  });
+}
+
+function nextExp(rate) {
+  var rnd = Math.random();
+  while (rnd === 0) { 
+    rnd = Math.random(); 
+  }
+  return -1 * Math.log(rnd)/rate;
+}
+
+
+function nextGaussian(mean, stddev) {
+  mean = mean || 0;
+  stddev = stddev || 1;
+  var s = 0, z0, z1;
+  while (s === 0 || s >= 1) {
+    z0 = 2 * Math.random() - 1;
+    z1 = 2 * Math.random() - 1;
+    s = z0*z0 + z1*z1;
+  }
+  return z0 * Math.sqrt(-2 * Math.log(s) / s) * stddev + mean;
+}
+
+function nextPareto(min, shape) {
+  shape = shape || 3;
+  var rnd = Math.random();
+  return Math.pow(1 - rnd, -1/shape) * min;
+}
+
+function nextBoundedPareto(min, max, shape) {
+  shape = shape || 0.1;
+  var l = 1, rnd = Math.random();
+  while (rnd === 0) { 
+    rnd = Math.random(); 
+  }
+  return min*max/Math.pow(Math.pow(max, shape)*(1-rnd) + Math.pow(min, shape)*rnd, 1/shape);
+}
+
